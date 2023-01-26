6f48059f5ca8437cb86129eda887c7346e5b2ed9
v20035017 <camille.vedani@outlook.fr>
2023-01-26 17:33:58 +0100

    Changes not staged for commit: 	modified:   Methode1.py 	modified:   Methode2.py

--- Methode3.py
+++ Methode3.py
@@ -1,4 +1,5 @@
 from random import randint
+import math
 Array=[]
 def GenerateRandomArray(size,lowerBound,upperBound):  
     Array=[randint(lowerBound,upperBound) for i in range(size)]
@@ -12,11 +13,26 @@ def GetSubArray(Array:list,firstIndex,lastIndex):
     sub=[Array[i] for i in range(firstIndex,lastIndex)]
     return sub
 
-def Somme(T:list,d,f):
-    if f<d:
-        return -1
-    sum =Somme(T,d,f-1)+T[f]
-    return sum
+#def Somme(T:list,d,f):
+#    if f<d:
+#        return -1
+#    sum =Somme(T,d,f-1)+T[f]
+#    return sum
+def Somme(Array):
+    Sum=0
+    if isinstance(Array,list):
+        for i in Array:
+            Sum+=i
+        return Sum
+    return Array
+
 
 def SousSequenceMaximal(Array:list):
-    rightSousSequence=
+    sum=Array[0]
+    pointer=math.floor(len(Array)/2)
+    rightSousSequence=GetSubArray(Array,0,pointer)
+    leftSousSequence=GetSubArray(Array,pointer,len(Array)-1)
+    if(Somme(rightSousSequence) > sum):
+                sum=Somme(rightSousSequence)
+                print("my sub array",GetSubArray(Array,i,j))
+                sousSequenceMaximal=[i,j]
